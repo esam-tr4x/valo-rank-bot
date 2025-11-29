24e86a74e99960739e1d6664585a8287d0415a4b
esam-tr4x <esamjbareen94@gmail.com>
2025-11-29 21:04:55 +0200

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 10000;
-
-app.get("/rank", (req, res) => {
-  res.send("Diamond 3 - 0RR");
-});
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
